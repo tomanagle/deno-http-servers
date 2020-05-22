7baa4c0886e33d42fa027ae718607e0d287aa182
Tom Nagle <tomanagle@gmail.com>
2020-05-22 10:11:07 +1000

    fix: comment

--- standard-library.ts
+++ standard-library.ts
@@ -1,5 +1,9 @@
-// deno run --allow-net --allow-read standard-library.ts
-// Docs: https://deno.land/std/http
+/*
+ * deno run --allow-net --allow-read standard-library.ts
+ * OR
+ * deno run --allow-net --allow-read https://raw.githubusercontent.com/tomanagle/deno-http-servers/master/standard-library.ts
+ * Docs: https://deno.land/std/http
+ */
 import { serve } from 'https://deno.land/std/http/server.ts'
 const s = serve({ port: 8000 })
 console.log(`🦕 Deno server running at http://localhost:8000/ 🦕`)
