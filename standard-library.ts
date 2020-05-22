942c920b8ac8c1a3af428d1fde04dae9b4b9b7d2
Tom Nagle <tomanagle@gmail.com>
2020-05-22 10:17:24 +1000

    fix: spacing

--- standard-library.ts
+++ standard-library.ts
@@ -5,8 +5,11 @@
  * Docs: https://deno.land/std/http
  */
 import { serve } from 'https://deno.land/std/http/server.ts'
-const s = serve({ port: 8000 })
+
+const s = serve({ port: 1234 })
+
 console.log(`🦕 Deno server running at http://localhost:8000/ 🦕`)
+
 for await (const req of s) {
   req.respond({ body: 'Hello from your first Deno server' })
 }
